267e506bb6fdf15d34e49e3f9157d4b501e8a2e5
Tilman Schlenker <tilman@mailbox.org>
2023-01-08 21:34:13 +0100

    correct the escaped citations from ulysses

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 DOC=index.md
 WORDCOUNT=`perl -ne 'if ($i > 1) { print } else { /^---/ && $i++ }' $DOC | wc -w | bc`
-pandoc --template template.tex --bibliography=bookends.bib --citeproc --csl=elsevier-harvard.csl --metadata link-citations=true --metadata link-bibliography=true -V colorlinks=true -V geometry:margin=3cm -V geometry:right=4cm -V fontsize=12pt -V linestretch=1.5 -V wordcount=$WORDCOUNT -f markdown -t pdf --toc -o paper.pdf $DOC bibliography.md
+perl -pe 's|!\\[(.*?)\\\\\\[(.*?)\\\\]]\\((.*?)\\)|![\1\2](\3)|' < $DOC > escaped.md
+pandoc --template template.tex --bibliography=bookends.bib --citeproc --csl=elsevier-harvard.csl --metadata link-citations=true --metadata link-bibliography=true -V colorlinks=true -V geometry:margin=3cm -V geometry:right=4cm -V fontsize=12pt -V linestretch=1.5 -V wordcount=$WORDCOUNT -f markdown -t pdf --toc -o paper.pdf escaped.md bibliography.md
 open paper.pdf
