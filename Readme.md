26d977840f7edd934b559e1e01e8ce368a417e65
Tilman Schlenker <tilman@mailbox.org>
2023-01-08 23:16:22 +0100

    filename handling

--- Readme.md
+++ Readme.md
@@ -10,6 +10,7 @@ Make sure it has the following front matter:
 	module: Module name
 	term: term
 	matrikelnummer: Student number
+	file: Filename to save to (pdf will be appended)
 	---
 
 Export whole MD with front matter to index.md, include all images
--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 DOC=index.md
 WORDCOUNT=`perl -ne 'if ($i > 1) { print } else { /^---/ && $i++ }' $DOC | wc -w | bc`
+REGEX='^file: (.*)$'
+FILEPARAM=`grep -E "$REGEX" $DOC`
+[[ $FILEPARAM =~ $REGEX ]]
+FILENAME="${BASH_REMATCH[1]}.pdf"
+echo "Saving to $FILENAME"
 perl -pe 's|!\\[(.*?)\\\\\\[(.*?)\\\\]]\\((.*?)\\)|![\1\2](\3)|' < $DOC > escaped.md
-pandoc --template template.tex --bibliography=bookends.bib --citeproc --csl=elsevier-harvard.csl --metadata link-citations=true --metadata link-bibliography=true -V colorlinks=true -V geometry:margin=3cm -V geometry:right=4cm -V fontsize=12pt -V linestretch=1.5 -V wordcount=$WORDCOUNT -V fontfamily=opensans -V fontfamilyoptions=default -f markdown -t pdf --toc -o paper.pdf escaped.md bibliography.md
-open paper.pdf
+pandoc --template template.tex --bibliography=bookends.bib --citeproc --csl=elsevier-harvard.csl --metadata link-citations=true --metadata link-bibliography=true -V colorlinks=true -V geometry:margin=3cm -V geometry:right=4cm -V fontsize=12pt -V linestretch=1.5 -V wordcount=$WORDCOUNT -V fontfamily=opensans -V fontfamilyoptions=default -f markdown -t pdf --toc -o $FILENAME escaped.md bibliography.md
+open $FILENAME
