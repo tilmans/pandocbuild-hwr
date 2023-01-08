93414bb8ecee63f7c198ff4c81891230924cc5d6
Tilman Schlenker <tilman@mailbox.org>
2023-01-08 18:29:52 +0100

    better wordcount

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 DOC=index.md
-WORDCOUNT=`wc -w < index.md | bc`
-pandoc --template template.tex --bibliography=bookends.bib --citeproc --csl=elsevier-harvard.csl -V geometry:margin=3cm -V geometry:right=4cm -V fontsize=12pt -V linestretch=1.5 -V wordcount=$WORDCOUNT -f markdown -t pdf --toc -o paper.pdf $DOC
+WORDCOUNT=`perl -ne 'if ($i > 1) { print } else { /^---/ && $i++ }' $DOC | wc -w | bc`
+pandoc --template template.tex --bibliography=bookends.bib --citeproc --csl=elsevier-harvard.csl -V geometry:margin=3cm -V geometry:right=4cm -V fontsize=12pt -V linestretch=1.5 -V wordcount=$WORDCOUNT -f markdown -t pdf --toc -o paper.pdf $DOC bibliography.md
 open paper.pdf
