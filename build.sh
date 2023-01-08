d13e971e3157e48b17560c9c8e147bd6212590d2
Tilman Schlenker <tilman@mailbox.org>
2023-01-08 22:18:24 +0100

    font fix

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 DOC=index.md
 WORDCOUNT=`perl -ne 'if ($i > 1) { print } else { /^---/ && $i++ }' $DOC | wc -w | bc`
 perl -pe 's|!\\[(.*?)\\\\\\[(.*?)\\\\]]\\((.*?)\\)|![\1\2](\3)|' < $DOC > escaped.md
-pandoc --template template.tex --bibliography=bookends.bib --citeproc --csl=elsevier-harvard.csl --metadata link-citations=true --metadata link-bibliography=true -V colorlinks=true -V geometry:margin=3cm -V geometry:right=4cm -V fontsize=12pt -V linestretch=1.5 -V wordcount=$WORDCOUNT -f markdown -t pdf --toc -o paper.pdf escaped.md bibliography.md
+pandoc --template template.tex --bibliography=bookends.bib --citeproc --csl=elsevier-harvard.csl --metadata link-citations=true --metadata link-bibliography=true -V colorlinks=true -V geometry:margin=3cm -V geometry:right=4cm -V fontsize=12pt -V linestretch=1.5 -V wordcount=$WORDCOUNT -V fontfamily=opensans -V fontfamilyoptions=default -f markdown -t pdf --toc -o paper.pdf escaped.md bibliography.md
 open paper.pdf
